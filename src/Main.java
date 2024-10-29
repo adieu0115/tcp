5da8ffbc781019a0626d1c3e818439d67dac7a8d
doubleDRG <dyl660115@gmail.com>
2024-10-29 11:26:52 +0900

    init1

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        try (ServerSocket serverSocket = new ServerSocket(1234))
+        {
+            while (true)
+            {
+                Socket socket = serverSocket.accept();
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                String message = reader.readLine();
+                socket.close();
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
