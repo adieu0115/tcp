e8cc05f32e0f823bcb134a5dd5bbd4618a7ab801
doubleDRG <dyl660115@gmail.com>
2024-10-30 22:02:58 +0900

    swap to udp

--- src/UdpServer.java
+++ src/UdpServer.java
@@ -7,15 +7,15 @@ import java.net.SocketException;
 public class UdpServer
 {
     // 0.반드시 cmd가 아닌, bash를 사용할 것.
-    // 1.echo "hello world" | ncat -u localhost 7070 명령어로 통신
-    // 2.printf "EXIT" | ncat -u localhost 7070 명령어로 종료
-    // ex. for i in {1..100}; do echo "Packet $i" | ncat -u localhost 7070; done
+    // 1.echo "hello world" | ncat -u localhost 9090 명령어로 통신
+    // 2.printf "EXIT" | ncat -u localhost 9090 명령어로 종료
+    // ex. for i in {1..100}; do echo "Packet $i" | ncat -u localhost 9090; done
     public void startServer()
     {
         DatagramSocket server = null;
         try
         {
-            server = new DatagramSocket(7070, InetAddress.getByName("0.0.0.0"));
+            server = new DatagramSocket(9090, InetAddress.getByName("0.0.0.0"));
             int bufferLength = 256;
             byte[] buffer = new byte[bufferLength];
             DatagramPacket packet = new DatagramPacket(buffer, bufferLength);
@@ -24,7 +24,6 @@ public class UdpServer
             {
                 System.out.println("Server:Waiting for request.");
                 server.receive(packet);
-                System.out.println("packet Test=" + packet);
                 int dataLength = packet.getLength();
                 System.out.println("Server:received. Data length=" + dataLength);
                 String data = new String(packet.getData(), 0, dataLength);
